87f441b13d9a12ba14e764fc63ae55a5ca063711
Shaun <seansama@pop-os.localdomain>
2023-03-20 15:23:01 +0300

    References for foreign keys

--- db/migrate/20230320121500_create_restaurant_pizzas.rb
+++ db/migrate/20230320121500_create_restaurant_pizzas.rb
@@ -2,7 +2,8 @@ class CreateRestaurantPizzas < ActiveRecord::Migration[6.1]
   def change
     create_table :restaurant_pizzas do |t|
       t.string :price
-
+      t.references :pizza, foreign_key: true
+      t.references :restaurant, foreign_key: true
       t.timestamps
     end
   end
