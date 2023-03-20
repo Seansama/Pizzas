9ade3715ba7f7f1a6bf9c599f8ebf59d1040459a
Shaun <seansama@pop-os.localdomain>
2023-03-20 15:43:52 +0300

    Updated resources in routes to only include needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :restaurant_pizzas
-  resources :restaurants
-  resources :pizzas
+  resources :restaurant_pizzas, only: [:create]
+  resources :restaurants, only: [:show, :destroy, :index]
+  resources :pizzas, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
