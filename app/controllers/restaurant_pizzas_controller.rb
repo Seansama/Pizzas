fe4bf0f1779de448832771e8616c94b1935a848c
Shaun <seansama@pop-os.localdomain>
2023-03-20 15:49:23 +0300

    Updated RestaurantPizzasController with Reusable private methods

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,12 @@
 class RestaurantPizzasController < ApplicationController
+
+
+
+  private
+  def restaurant_pizza_params
+    params.permit(:restaurant_id, :pizza_id, :price)
+  end
+  def unprocessable_entity_method(e)
+    render json: {errors: e.record.errors}, status: :unprocessable_entity
+  end
 end
